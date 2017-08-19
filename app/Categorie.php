73467d1d12ad4cd928b96a7781aba9667b4774a5
Philip Paredis <philipparedis@gmail.com>
2017-08-19 12:21:06 +0200

    databank volledig oké

--- app/Categorie.php
+++ app/Categorie.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Categorie extends Model
 {
-    //
+    protected $fillable = ['categoryName'];
+	protected $primaryKey = 'categoryID';
+	
+	public function tickets() {
+		return $this->hasMany(Ticket::class);
+	}
 }
